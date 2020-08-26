5e4e5d544a28ef74f610a9935287345489eab53f
mr-winner <developer.winner@gmail.com>
2020-08-25 21:38:45 -0300

    sessao produtos sem carrossel

--- scripts/sao-geraldo-main.js
+++ scripts/sao-geraldo-main.js
@@ -1,8 +1,22 @@
+let produtos = [{
+  nome: "Pão de Queijo",
+  peso: ["100g", "3kg", "10kg"],
+  descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet massa eget nisi condimentum efficitur a sollicitudin metus. Aliquam erat volutpat. Pellentesque dictum at libero sit amet venenatis. Suspendisse maximus metus mollis dui euismod, et tincidunt felis egestas."
+}]
+
+
+
+
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
+  $(".main-carousel.owl-carousel").owlCarousel({
       nav:true,
       margin:15,
       items: 1,
       }
-  );  
+  );
+  
+      console.log(produtos)
+
+
+
 })
