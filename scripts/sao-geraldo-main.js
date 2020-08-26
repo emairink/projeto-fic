a876d50c26f071316205961b451d46709ace26c4
mr-winner <developer.winner@gmail.com>
2020-08-26 14:52:12 -0300

    finalizado(nao-responsivo)

--- scripts/sao-geraldo-main.js
+++ scripts/sao-geraldo-main.js
@@ -4,19 +4,12 @@ let produtos = [{
   descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet massa eget nisi condimentum efficitur a sollicitudin metus. Aliquam erat volutpat. Pellentesque dictum at libero sit amet venenatis. Suspendisse maximus metus mollis dui euismod, et tincidunt felis egestas."
 }]
 
-
-
-
 $(document).ready(function(){
   $(".main-carousel.owl-carousel").owlCarousel({
       nav:true,
-      margin:15,
+      margin:25,
       items: 1,
       }
   );
   
-      console.log(produtos)
-
-
-
 })
